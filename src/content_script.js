a0740ac2dfcb3286d771f9f0b6c2553d5dcbf801
miyashiro <daisuke@prince-engineering.jp>
2020-12-13 16:09:48 +0900

    タスクボード上でIssueを選択した場合に、Issueの詳細に子Issueの一覧を表示できるように

--- src/content_script.js
+++ src/content_script.js
@@ -1,11 +1,32 @@
 ﻿//----------------------
 $(function () {
-    //↓疎通確認コード：タイトルの■が回転するはず
-    console.log('Chrome拡張の疎通確認コードが動いています。動作確認ができたら削除してください。');
-    let flag = false;
-    setInterval(function () {
-        document.title = (flag ? "■" : "◆") + document.title.replace(/^[■◆]/, '');
-        flag = !flag;
-    }, 1000);
-    //↑疎通確認コード
+  const target = document.getElementsByClassName('js-project-card-details')[0];
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      $.get(mutation.target.getAttribute('data-issue-url'), function(data){
+        var result = data.match( /<[^<]+data-hovercard-type="issue"[^>]+>[^>]+>/g );
+        if (result) { 
+          var childrenDiv = $.parseHTML('<div id="target-children" style="padding: 20px;" class="border-top">')[0];
+          childrenDiv.append($.parseHTML('<div style="font-weight: bold;">子issue</div>')[0]);
+          $.each(result, function(i, txt) {
+            var textPart = $.parseHTML(txt)[0];
+            var textPartDiv = $.parseHTML('<div>')[0];
+            textPart.setAttribute("style", "font-size: 10px!important;");
+            textPartDiv.append(textPart);
+            childrenDiv.append(textPartDiv);
+          });
+          $('.project-comment-title-hover').after(childrenDiv);
+        };
+        console.log(result);
+      });
+    });
+  });
+
+  // オブザーバの設定
+  const config = {
+    attributes: true
+  };
+
+  // 対象ノードとオブザーバの設定を渡す
+  observer.observe(target, config);
 });
